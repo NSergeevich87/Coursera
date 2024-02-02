bd0a75129be3fc718709918355da5eea6853e32f
NSergeevich87 <niknikitaser@gmail.com>
2024-02-02 19:53:53 +0100

    DeleteSomeProjects

--- Exercise7/Intermediate/Build/Win64/x64/Exercise7Editor/Development/Core/SharedPCH.Core.RTTI.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.36.32543
-#include "D:/Unreal/Projects/Coursera/Exercise7/Intermediate/Build/Win64/x64/Exercise7Editor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h"
--- Exercise7/Intermediate/Build/Win64/x64/Exercise7Editor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.36.32543
-#include "D:/Unreal/Projects/Coursera/Exercise7/Intermediate/Build/Win64/x64/Exercise7Editor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h"
